dd54d7b723672523e4dec7cc95615ee9c48e830f
hamed mohamed <72640424+HamedMohamed99@users.noreply.github.com>
2022-06-29 18:23:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1066,7 +1066,7 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=false)
 
 
 def errorhandler(e):
